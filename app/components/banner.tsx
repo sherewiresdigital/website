c3af8c209defecf80afbbbfb91e906e1b9b91b10
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2024-01-18 11:02:45 -0500

    add icons

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,10 +1,16 @@
+import CampaignOutlinedIcon from '@mui/icons-material/CampaignOutlined';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 export default function Banner() {
   return <div className="container py-8">
     <div className="flex mx-auto columns-2 items-center w-10/12">
       <div className="px-24">
-        <h2 className="text-lg font-medium">
-          Upcoming
-        </h2>
+        <div className='flex'>
+          <h2 className="text-lg font-medium">
+            Upcoming
+          </h2>
+          <CampaignOutlinedIcon className='ml-4 w-8 h-8 font-light'/>
+        </div>
         <h1 className="pt-6 text-6xl font-bold">
           STEP IN HIGH
         </h1>
@@ -15,10 +21,11 @@ export default function Banner() {
           Oct 12, 2023 Online
         </h3>
         <div className="pt-12">
-        <button className="rounded-3xl border-2 border-slate-500 px-4 py-2">
-          <span className="text-base font-medium">Register for free</span>
-        </button>
-      </div>
+          <button className="rounded-3xl border-2 border-slate-500 px-4 py-2">
+            <span className="text-base font-medium">Register for free</span>
+            <ArrowForwardIcon className='ml-4' />
+          </button>
+        </div>
       </div>
       <div className="px-24">
         flyer
--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,15 @@
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+
 export default function Footer() {
   return <div className="container py-8">
     <div className="flex mx-auto columns-2 items-start w-10/12">
       <div className="flex-column justify-start px-24 w-2/3">
-        <h1 className="text-3xl font-medium">
-          Join our community
-        </h1>
+        <div className='flex items-center'>
+          <h1 className="text-3xl font-medium">
+            Join our community
+          </h1>
+          <ArrowCircleRightOutlinedIcon className='ml-4 w-8 h-8 -rotate-45' />
+        </div>
         <h6 className="pt-6 text-base font-normal w-96">
           Join us today to connect, learn, and thrive in an
           environment where your brilliance shines and your
